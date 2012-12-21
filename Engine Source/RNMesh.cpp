771206a988f842f74ebce97945c002cc74aa92eb
Sidney Just <justsid@widerwille.com>
2012-12-21 05:25:24 +0100

    Added a simple mesh class with support for multiple LOD stages.

---
+++ Engine Source/RNMesh.cpp
@@ -0,0 +1,195 @@
+//
+//  RNMesh.cpp
+//  Rayne
+//
+//  Copyright 2012 by Felix Pohl and Sidney Just. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#include "RNMesh.h"
+
+namespace RN
+{
+	MeshLODStage::MeshLODStage(const Array<MeshDescriptor>& descriptor)
+	{
+		_descriptor = Array<MeshDescriptor>(descriptor);
+		_meshSize = 0;
+		_meshData = 0;
+		
+		_vbo = _ivbo = -1;
+		
+		for(int i=0; i<_descriptor.Count(); i++)
+		{
+			size_t size = _descriptor[i].elements * SizeForMeshFeatureType(_descriptor[i].type);
+			
+			_meshSize += size;
+			_descriptor[i]._pointer = (uint8 *)malloc(size);
+			_descriptor[i]._offset = 0;
+		}
+	}
+	
+	MeshLODStage::~MeshLODStage()
+	{
+		if(_vbo != -1)
+			glDeleteBuffers(1, &_vbo);
+		
+		if(_ivbo != -1)
+			glDeleteBuffers(1, &_ivbo);
+		
+		for(int i=0; i<_descriptor.Count(); i++)
+		{
+			if(_descriptor[i]._pointer)
+				free(_descriptor[i]._pointer);
+		}
+		
+		if(_meshData)
+			free(_meshData);
+	}
+	
+	void MeshLODStage::GenerateMesh()
+	{
+		if(!_meshData)
+			_meshData = malloc(_meshSize);
+		
+		uint8 *bytes = (uint8 *)_meshData;
+		
+		for(int i=0; i<_meshSize;)
+		{
+			for(int j=0; j<_descriptor.Count(); j++)
+			{
+				if(_descriptor[i].feature == kMeshFeatureIndices)
+				{
+					_indices = _descriptor[i]._pointer;
+					_indicesSize = _descriptor[i].elements * sizeof(uint16);
+					continue;
+				}
+				
+				switch(_descriptor[i].type)
+				{
+					case kMeshFeatureTypeVector2:
+					{
+						const size_t size = 2 * sizeof(float);
+						
+						memcpy(&bytes[i], &_descriptor[i]._pointer[_descriptor[i]._offset], size);
+						
+						_descriptor[i]._offset += size;
+						i += size;
+						break;
+					}
+						
+					case kMeshFeatureTypeVector3:
+					{
+						const size_t size = 3 * sizeof(float);
+						
+						memcpy(&bytes[i], &_descriptor[i]._pointer[_descriptor[i]._offset], size);
+						
+						_descriptor[i]._offset += size;
+						i += size;
+						break;
+					}
+						
+					case kMeshFeatureTypeVector4:
+					{
+						const size_t size = 4 * sizeof(float);
+						
+						memcpy(&bytes[i], &_descriptor[i]._pointer[_descriptor[i]._offset], size);
+						
+						_descriptor[i]._offset += size;
+						i += size;
+						break;
+					}
+						
+					case kMeshFeatureTypeUint16:
+					{
+						const size_t size = sizeof(uint16);
+						
+						memcpy(&bytes[i], &_descriptor[i]._pointer[_descriptor[i]._offset], size);
+						
+						_descriptor[i]._offset += size;
+						i += size;
+						break;
+					}
+						
+					default:
+						break;
+				}
+			}
+		}
+		
+		
+		// Generate the buffers
+		if(_vbo == -1)
+			glGenBuffers(1, &_vbo);
+			
+		if(_ivbo == -1)
+			glGenBuffers(1, &_ivbo);
+		
+		
+		glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+		glBufferData(GL_ARRAY_BUFFER, _meshSize, _meshData, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ivbo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, _indicesSize, _indices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+	
+	size_t MeshLODStage::SizeForMeshFeatureType(MeshFeatureType type)
+	{
+		switch(type)
+		{
+			case kMeshFeatureTypeVector2:
+				return 2 * sizeof(float);
+				break;
+				
+			case kMeshFeatureTypeVector3:
+				return 3 * sizeof(float);
+				break;
+				
+			case kMeshFeatureTypeVector4:
+				return 4 * sizeof(float);
+				
+			case kMeshFeatureTypeUint16:
+				return sizeof(uint16);
+				break;
+			
+			default:
+				return 0;
+		}
+	}
+	
+	
+	Mesh::Mesh()
+	{
+	}
+	
+	Mesh::~Mesh()
+	{
+		for(int i=0; i<_LODStages.Count(); i++)
+		{
+			delete _LODStages[i];
+		}
+	}
+	
+	MeshLODStage *Mesh::AddLODStage(const Array<MeshDescriptor>& descriptor)
+	{
+		MeshLODStage *stage = new MeshLODStage(descriptor);
+		_LODStages.AddObject(stage);
+		
+		return stage;
+	}
+	
+	MeshLODStage *Mesh::LODStage(int index)
+	{
+		return _LODStages[index];
+	}
+	
+	void Mesh::UpdateMesh()
+	{
+		for(int i=0; i<_LODStages.Count(); i++)
+		{
+			_LODStages[i]->GenerateMesh();
+		}
+	}
+}
---
+++ Engine Source/RNMesh.h
@@ -0,0 +1,104 @@
+//
+//  RNMesh.h
+//  Rayne
+//
+//  Copyright 2012 by Felix Pohl and Sidney Just. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#ifndef __RAYNE_MESH_H__
+#define __RAYNE_MESH_H__
+
+#include "RNBase.h"
+#include "RNObject.h"
+#include "RNArray.h"
+#include "RNVector.h"
+
+namespace RN
+{
+	typedef enum
+	{
+		kMeshFeatureVertices = 0,
+		kMeshFeatureColor0 = 1,
+		kMeshFeatureColor1 = 2,
+		kMeshFeatureUVSet0 = 3,
+		kMeshFeatureUVSet1 = 4,
+		kMeshFeatureIndices = 5,
+		
+		kMaxMeshFeatures = 6
+	} MeshFeature;
+	
+	typedef enum
+	{
+		kMeshFeatureTypeVector2,
+		kMeshFeatureTypeVector3,
+		kMeshFeatureTypeVector4,
+		kMeshFeatureTypeUint16
+	} MeshFeatureType;
+	
+	class MeshLODStage;
+	struct MeshDescriptor
+	{
+	friend class MeshLODStage;
+	public:
+		MeshFeature feature;
+		MeshFeatureType type;
+		
+		int32 elements;
+		
+	private:
+		uint8 *_pointer;
+		size_t _offset;
+		
+		bool _dirty;
+	};
+	
+	class MeshLODStage
+	{
+	public:
+		MeshLODStage(const Array<MeshDescriptor>& descriptor);
+		~MeshLODStage();
+		
+		template <typename T>
+		T *Data(int32 index) const
+		{
+			return static_cast<T *>(_descriptor[index]._pointer);
+		}
+		
+		void GenerateMesh();
+		
+	private:
+		static size_t SizeForMeshFeatureType(MeshFeatureType type);
+		
+		bool _vboToggled;
+		
+		GLuint _vbo;
+		GLuint _ivbo;
+		
+		size_t _meshSize;
+		size_t _indicesSize;
+		
+		void *_meshData;
+		void *_indices;
+		Array<MeshDescriptor> _descriptor;
+	};
+	
+	class Mesh : public Object
+	{
+	public:
+		Mesh();
+		virtual ~Mesh();
+		
+		MeshLODStage *AddLODStage(const Array<MeshDescriptor>& descriptor);
+		
+		MeshLODStage *LODStage(int index);
+		machine_uint LODStages() const { return _LODStages.Count(); }
+		
+		void UpdateMesh();
+		
+	private:
+		Array<MeshLODStage *> _LODStages;
+	};
+}
+
+#endif /* __RAYNE_MESH_H__ */
