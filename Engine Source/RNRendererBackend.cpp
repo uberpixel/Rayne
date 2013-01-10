2741e3a6c3f0c322190ee8c5d47aa17c307c3c44
Sidney Just <justsid@widerwille.com>
2013-01-10 16:16:26 +0100

    Welcome aboard Nils… :)

--- Engine Source/RNRendererBackend.cpp
+++ Engine Source/RNRendererBackend.cpp
@@ -2,7 +2,7 @@
 //  RNRendererBackend.cpp
 //  Rayne
 //
-//  Copyright 2013 by Felix Pohl and Sidney Just. All rights reserved.
+//  Copyright 2013 by Felix Pohl, Nils Daumann and Sidney Just. All rights reserved.
 //  Unauthorized use is punishable by torture, mutilation, and vivisection.
 //
 
