aafbde5e7782ce26542304ad04367fb5491cf134
Sidney Just <justsid@widerwille.com>
2013-06-30 18:08:23 +0200

    Added an AttributedString class which allows the addition of attributes to ranges within the wrapped string (eg. to describe the font to use for a given subrange of the string when it's displayed on the screen)

---
+++ Engine Source/RNAttributedString.cpp
@@ -0,0 +1,70 @@
+//
+//  RNAttributedString.cpp
+//  Rayne
+//
+//  Copyright 2013 by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#include "RNAttributedString.h"
+
+namespace RN
+{
+	AttributedString::AttributedString(class String *string)
+	{
+		_string = string->Retain();
+		_editing = false;
+	}
+	AttributedString::~AttributedString()
+	{
+		_string->Release();
+	}
+	
+	
+	void AttributedString::BeginEditing()
+	{
+		_editing = true;
+	}
+	
+	void AttributedString::EndEditing()
+	{
+		_editing = false;
+		ApplyUpdates();
+	}
+	
+	void AttributedString::AddAttribute(class String *key, Object *value, const Range& range)
+	{
+		_queuedAttributes.emplace_back(stl::interval_tree<Attribute>::interval(range, Attribute(key, value)));
+		
+		if(!_editing)
+			ApplyUpdates();
+	}
+	
+	void AttributedString::ApplyUpdates()
+	{
+		if(!_queuedAttributes.empty())
+		{
+			_attributes.find_overlapping(Range(_attributes.min(), (_attributes.max() - _attributes.min())), _queuedAttributes);
+			_attributes = stl::interval_tree<Attribute>(std::move(_queuedAttributes));
+			
+			_queuedAttributes = std::vector<stl::interval_tree<Attribute>::interval>();
+		}
+	}
+	
+	Dictionary *AttributedString::AttributesAtIndex(size_t index)
+	{
+		std::vector<stl::interval_tree<Attribute>::interval> overlapping;
+		_attributes.find_overlapping(Range(index, 1), overlapping);
+		
+		if(overlapping.empty())
+			return (new Dictionary())->Autorelease();
+		
+		Dictionary *attributes = new Dictionary(overlapping.size());
+		for(auto i=overlapping.begin(); i!=overlapping.end(); i++)
+		{
+			attributes->SetObjectForKey(i->value.value, i->value.key);
+		}
+		
+		return attributes->Autorelease();
+	}
+}
---
+++ Engine Source/RNAttributedString.h
@@ -0,0 +1,71 @@
+//
+//  RNAttributedString.h
+//  Rayne
+//
+//  Copyright 2013 by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#ifndef __RAYNE_ATTRIBUTEDSTRING_H__
+#define __RAYNE_ATTRIBUTEDSTRING_H__
+
+#include "RNBase.h"
+#include "RNString.h"
+#include "RNDictionary.h"
+#include "RNIntervalTree.h"
+
+namespace RN
+{
+	class AttributedString : public Object
+	{
+	public:
+		RNAPI AttributedString(String *string);
+		RNAPI ~AttributedString() override;
+		
+		RNAPI void BeginEditing();
+		RNAPI void EndEditing();
+		
+		RNAPI void AddAttribute(String *key, Object *value, const Range& range);
+		RNAPI Dictionary *AttributesAtIndex(size_t index);
+		
+		size_t Length() const { return _string->Length(); }
+		String *String() const { return _string; }
+		
+	private:
+		class Attribute
+		{
+		public:
+			Attribute(class String *tkey, Object *tvalue)
+			{
+				key = tkey->Retain();
+				value = tvalue->Retain();
+			}
+			
+			Attribute(const Attribute& other)
+			{
+				key = other.key->Retain();
+				value = other.value->Retain();
+			}
+			
+			~Attribute()
+			{
+				key->Release();
+				value->Release();
+			}
+			
+			class String *key;
+			Object *value;
+		};
+		
+		void ApplyUpdates();
+		
+		class String *_string;
+		stl::interval_tree<Attribute> _attributes;
+		
+		bool _editing;
+		std::vector<stl::interval_tree<Attribute>::interval> _queuedAttributes;
+		
+	};
+}
+
+#endif /* __RAYNE_ATTRIBUTEDSTRING_H__ */
