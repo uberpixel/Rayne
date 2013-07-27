a35a6ead814e515a47d907e10dd2f64f1230b51d
Sidney Just <justsid@widerwille.com>
2013-07-27 10:10:41 +0200

    Removed the Optimized schemes and changed the build scripts around a bit.

--- Engine Host/OS X/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p "$PROJECT_DIR/../../Build/"
-
-rm -rf "$PROJECT_DIR/../../Build/$EXECUTABLE_NAME.app"
-cp -r "$BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME.app" "$PROJECT_DIR/../../Build/"
-
-cd "$PROJECT_DIR/../../Engine Test/Game"
-xcrun xcodebuild -project Game-osx.xcodeproj -configuration $CONFIGURATION
-rm -rf "$PROJECT_DIR/../../Engine Test/Game/build"
