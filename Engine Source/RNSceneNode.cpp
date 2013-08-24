0c2d045cb95bbae00758a2be8bb2752acb9c8868
Nils Daumann <nils_daumann@slindev.com>
2013-08-24 16:24:08 +0200

    disabled view frustum culling

--- Engine Source/RNSceneNode.cpp
+++ Engine Source/RNSceneNode.cpp
@@ -68,7 +68,7 @@ namespace RN
 	
 	bool SceneNode::IsVisibleInCamera(Camera *camera)
 	{
-		return camera->InFrustum(_transformedBoundingSphere);
+		return true;//camera->InFrustum(_transformedBoundingSphere);
 	}
 	
 	void SceneNode::Render(Renderer *renderer, Camera *camera)
