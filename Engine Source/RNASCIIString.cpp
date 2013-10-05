3cb0da26017b734c41628ed38ebbd4e3845abdeb
Sidney Just <justsid@widerwille.com>
2013-10-05 05:15:02 +0200

    Fixed a buffer overflow bug in the ASCIIString() constructor

--- Engine Source/RNASCIIString.cpp
+++ Engine Source/RNASCIIString.cpp
@@ -150,7 +150,7 @@ namespace RN
 		_string = new char[_length + 1];
 		_string[_length] = '\0';
 		
-		strcpy(_string, string);
+		strlcpy(_string, string, length);
 	}
 	
 	ASCIIString::ASCIIString(UniChar *string)
