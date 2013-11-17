a07248f362f2795fea1ef9ec181afacff8a32b05
Sidney Just <justsid@widerwille.com>
2013-11-17 10:31:19 +0100

    Added a SHA256 implementation

---
+++ Engine Source/RNSHA2.h
@@ -0,0 +1,45 @@
+//
+//  RNSHA2.h
+//  Rayne
+//
+//  Copyright 2013 by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#ifndef __RAYNE_SHA2_H__
+#define __RAYNE_SHA2_H__
+
+#include "RNBase.h"
+
+namespace RN
+{
+	namespace stl
+	{
+		class sha2_context
+		{
+		public:
+			sha2_context();
+			
+			void update(const std::vector<uint8>& input);
+			void update(const uint8 *input, size_t size);
+			
+			void finish(std::vector<uint8>& result);
+			
+		private:
+			void process(const uint8 *data);
+			
+			uint64 _total;
+			uint32 _state[8];
+			uint8 _buffer[64];
+		};
+		
+		static inline void sha2(const uint8 *input, size_t length, std::vector<uint8>& result)
+		{
+			sha2_context context;
+			context.update(input, length);
+			context.finish(result);
+		}
+	}
+}
+
+#endif /* __RAYNE_SHA2_H__ */
