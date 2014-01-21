1c1d62fb08afcc5f2eab951f37a755c79fbd85c5
Sidney Just <justsid@widerwille.com>
2014-01-21 07:25:14 +0100

    0.6 development branch kick off :)

--- Engine Source/RNBase.h
+++ Engine Source/RNBase.h
@@ -110,8 +110,8 @@
 // ---------------------------
 
 #define kRNVersionMajor 0
-#define kRNVersionMinor 5
-#define kRNVersionPatch 5
+#define kRNVersionMinor 6
+#define kRNVersionPatch 0
 
 #define kRNABIVersion 4
 
