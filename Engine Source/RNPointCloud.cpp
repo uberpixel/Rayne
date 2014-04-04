71dfdb9e2a4714987f5ded7a67cc457ce0a55547
Nils Daumann <nils_daumann@slindev.com>
2014-04-05 00:15:00 +0200

    Added a point cloud class.

---
+++ Engine Source/RNPointCloud.cpp
@@ -0,0 +1,17 @@
+//
+//  RNPointCloud.cpp
+//  Rayne
+//
+//  Copyright 2014 by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#include "RNPointCloud.h"
+
+namespace RN
+{
+	PointCloud::PointCloud()
+	{
+		
+	}
+}
---
+++ Engine Source/RNPointCloud.h
@@ -0,0 +1,32 @@
+//
+//  RNPointCloud.h
+//  Rayne
+//
+//  Copyright 2014 by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#ifndef __Rayne__RNPointCloud__
+#define __Rayne__RNPointCloud__
+
+#include "RNVector.h"
+
+namespace RN
+{
+	struct Point
+	{
+	public:
+		Vector3 position;
+	};
+	
+	class PointCloud
+	{
+	public:
+		PointCloud();
+		
+	private:
+		
+	};
+}
+
+#endif /* defined(__Rayne__RNPointCloud__) */
