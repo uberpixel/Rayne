5b1ce346611ef452c2ae429c2d20e7d124995f71
Sidney Just <justsid@widerwille.com>
2014-07-31 21:20:20 +0200

    AddMetaClass() now also puts the name of the duplicate meta class into the exception name

--- Engine Source/RNCatalogue.cpp
+++ Engine Source/RNCatalogue.cpp
@@ -84,7 +84,7 @@ namespace RN
 	{
 		auto iterator = _metaClasses.find(meta->GetFullname());
 		if(iterator != _metaClasses.end())
-			throw Exception(Exception::Type::InvalidArgumentException, "A MetaClass of the same name already exists!");
+			throw Exception(Exception::Type::InvalidArgumentException, "A MetaClass of the same name (%s) already exists!", meta->GetFullname().c_str());
 		
 		_metaClasses.insert(std::unordered_map<std::string, MetaClass *>::value_type(meta->GetFullname(), meta));
 	}
