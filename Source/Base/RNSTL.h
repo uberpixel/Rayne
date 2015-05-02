2c7930aa48b6bd197ef9518602a456d50d4e9893
Sidney Just <justsid@widerwille.com>
2015-05-02 02:31:28 +0200

    Bits of clean up

--- Source/Base/RNSTL.h
+++ Source/Base/RNSTL.h
@@ -13,51 +13,6 @@ namespace RN
 {
 	namespace stl
 	{
-		static inline std::string html_encode(const std::string &string)
-		{
-			std::string buffer;
-			buffer.reserve(string.size() * 1.1f);
-			
-			for(size_t i = 0; i < string.size(); i ++)
-			{
-				switch(string[i])
-				{
-					case '&':
-						buffer.append("&amp;");
-						break;
-					case '\"':
-						buffer.append("&quot;");
-						break;
-					case '\'':
-						buffer.append("&apos;");
-						break;
-					case '<':
-						buffer.append("&lt;");
-						break;
-					case '>':
-						buffer.append("&gt;");
-						break;
-					case '\n':
-						buffer.append("<br />");
-						break;
-					case '\t':
-						buffer.append("&emsp;");
-						break;
-					default:
-						buffer.append(1, string[i]);
-						break;
-				}
-			}
-			
-			return buffer;
-		}
-		
-		static inline void html_encode(std::string &string)
-		{
-			std::string buffer = std::move(html_encode(const_cast<const std::string&>(string)));
-			std::swap(buffer, string);
-		}
-		
 		template<class T>
 		class lockable_shim
 		{
@@ -73,12 +28,12 @@ namespace RN
 			
 			void lock()
 			{
-				return _lock.Lock();
+				_lock.Lock();
 			}
 			
 			void unlock()
 			{
-				return _lock.Unlock();
+				_lock.Unlock();
 			}
 			
 		private:
