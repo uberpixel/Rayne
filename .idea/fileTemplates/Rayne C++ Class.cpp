c0bdc22c4831d597ea4a1f9b40a1a95a141b5ef1
Sidney Just <justsid@widerwille.com>
2015-06-09 12:42:40 +0200

    Added file templates for Rayne C++ and C++ header files

---
+++ .idea/fileTemplates/Rayne C++ Class.cpp
@@ -0,0 +1,13 @@
+//
+//  ${NAME}.cpp
+//  Rayne
+//
+//  Copyright ${YEAR} by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#[[#include]]# "${NAME}.h"
+
+namespace RN
+{
+}
