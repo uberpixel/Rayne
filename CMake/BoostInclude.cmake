26a5d1aefa8432e92cb588e7259cabf0085ec9df
Sidney Just <justsid@widerwille.com>
2015-11-01 15:15:14 +0000

    Reworked boost include system

--- CMake/BoostInclude.cmake
+++ CMake/BoostInclude.cmake
@@ -1,5 +1,5 @@
 
-macro(boost_include _NAME _INCLUDE_DIRS)
+macro(find_boost_library _NAME)
 
     set(_PATH ${VENDOR_BOOST_DIR}/${_NAME}/include)
 
@@ -7,6 +7,6 @@ macro(boost_include _NAME _INCLUDE_DIRS)
         message(FATAL_ERROR "Could not find boost::${_NAME}")
     endif()
 
-    set(_INCLUDE_DIRS ${_INCLUDE_DIRS} ${_PATH})
+    set(BOOST_${_NAME}_INCLUDE ${_PATH})
 
 endmacro()
