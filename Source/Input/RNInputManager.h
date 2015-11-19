c4e82347b09be9ea98c897c3c39bfe665b38ea65
Sidney Just <justsid@widerwille.com>
2015-11-19 14:11:10 +0000

    Compile error fix

--- Source/Input/RNInputManager.h
+++ Source/Input/RNInputManager.h
@@ -62,7 +62,7 @@ namespace RN
 		RNAPI bool IsControlToggling(const String *name) const;
 		RNAPI Object *GetControlValue(const String *name) const;
 
-		RNAPI const Vector2 &GetMouseDelta() const { return _mouseDelta; }
+		RNAPI const Vector3 &GetMouseDelta() const { return _mouseDelta; }
 
 	private:
 		InputManager();
