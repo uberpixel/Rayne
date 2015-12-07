2c8e5a8907a3c2ade2cf3d5a84b6e5468a7bc7f0
Sidney Just <justsid@widerwille.com>
2015-12-07 00:23:02 +0000

    Added file template for the Test Game (CLion)

---
+++ .idea/fileTemplates/Test Game C++ Class.cpp
@@ -0,0 +1,13 @@
+//
+//  ${NAME}.cpp
+//  Test Game
+//
+//  Copyright ${YEAR} by Überpixel. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and vivisection.
+//
+
+#[[#include]]# "${NAME}.h"
+
+namespace TG
+{
+}
