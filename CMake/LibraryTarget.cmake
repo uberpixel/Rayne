161eebc9c690da46915a9a71bbb59cac39d66bd3
Sidney Just <justsid@widerwille.com>
2016-02-12 03:14:42 +0000

    Removed the x86 and static library targets

--- CMake/LibraryTarget.cmake
+++ CMake/LibraryTarget.cmake
@@ -1,10 +1,6 @@
 
-macro(__rayne_create_target _NAME _ARCH _TYPE _SOURCES _HEADERS _RAYNE_LIBRARIES _VERSION _ABI)
+macro(__rayne_create_target _NAME _TYPE _SOURCES _HEADERS _RAYNE_LIBRARIES _VERSION _ABI)
     # Input check
-    if(NOT ((${_ARCH} EQUAL 64) OR (${_ARCH} EQUAL 32)))
-        message(FATAL_ERROR "Target must be either \"32\" or \"64\", is \"${_ARCH}\"")
-    endif()
-
     if(NOT (("${_TYPE}" STREQUAL "STATIC") OR ("${_TYPE}" STREQUAL "SHARED")))
         message(FATAL_ERROR "Type must be either \"STATIC\" or \"SHARED\", is \"${_TYPE}\"")
     endif()
@@ -14,12 +10,6 @@ macro(__rayne_create_target _NAME _ARCH _TYPE _SOURCES _HEADERS _RAYNE_LIBRARIES
 
     if("${_TYPE}" STREQUAL "STATIC")
         set(TARGET_NAME "${TARGET_NAME}-static")
-    endif()
-
-    if(${_ARCH} EQUAL 64)
-        set(TARGET_NAME "${TARGET_NAME}-x64")
-    else()
-        set(TARGET_NAME "${TARGET_NAME}-x86")
     endif()
 
     # Create the target
@@ -42,11 +32,7 @@ macro(__rayne_create_target _NAME _ARCH _TYPE _SOURCES _HEADERS _RAYNE_LIBRARIES
     #set_target_properties("${TARGET_NAME}" PROPERTIES PUBLIC_HEADER "${_HEADERS}")
 
     if(UNIX)
-        if(${_ARCH} EQUAL 64)
-            target_compile_options(${TARGET_NAME} PUBLIC -m64)
-        else()
-            target_compile_options(${TARGET_NAME} PUBLIC -m32)
-        endif()
+        target_compile_options(${TARGET_NAME} PUBLIC -m64)
     endif()
 
     if(NOT ("${_HEADERS}" STREQUAL ""))
@@ -64,12 +50,6 @@ macro(__rayne_create_target _NAME _ARCH _TYPE _SOURCES _HEADERS _RAYNE_LIBRARIES
                 set(LIBRARY_NAME "${LIBRARY_NAME}-static")
             endif()
 
-            if(${_ARCH} EQUAL 64)
-                set(LIBRARY_NAME "${LIBRARY_NAME}-x64")
-            else()
-                set(LIBRARY_NAME "${LIBRARY_NAME}-x86")
-            endif()
-
             target_link_libraries("${TARGET_NAME}" ${LIBRARY_NAME})
         endforeach()
     endif()
@@ -78,52 +58,17 @@ endmacro()
 
 macro(__rayne_target_names _NAME _NAMES)
 
-    if(APPLE)
-
-        set(${_NAMES}
-                "${_NAME}-x64"
-                "${_NAME}-static-x64")
-
-    else()
-
-        set(${_NAMES}
-                "${_NAME}-x64"
-                "${_NAME}-static-x64"
-                "${_NAME}-x86"
-                "${_NAME}-static-x86")
-
-    endif()
+    set(${_NAMES}
+            "${_NAME}")
 
 endmacro()
 
 macro(rayne_add_library _NAME _SOURCES _HEADERS _RAYNE_LIBRARIES _VERSION _ABI)
 
-    if(APPLE)
+    __rayne_create_target(${_NAME} SHARED "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
 
-        __rayne_create_target(${_NAME} 64 SHARED "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-        __rayne_create_target(${_NAME} 64 STATIC "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-
-        set(LIBRARIES
-                "${_NAME}-x64"
-                "${_NAME}-static-x64")
-
-    else()
-
-        __rayne_create_target(${_NAME} 32 SHARED "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-        __rayne_create_target(${_NAME} 32 STATIC "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-
-        __rayne_create_target(${_NAME} 64 SHARED "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-        __rayne_create_target(${_NAME} 64 STATIC "${_SOURCES}" "${_HEADERS}" "${_RAYNE_LIBRARIES}" "${_VERSION}" "${_ABI}")
-
-        set(LIBRARIES
-                "${_NAME}-x64"
-                "${_NAME}-static-x64"
-                "${_NAME}-x86"
-                "${_NAME}-static-x86")
-
-    endif()
-
-    add_custom_target(${_NAME})
+    set(LIBRARIES
+            "${_NAME}")
 
     foreach(LIBRARY ${LIBRARIES})
         add_dependencies(${_NAME} "${LIBRARY}")
