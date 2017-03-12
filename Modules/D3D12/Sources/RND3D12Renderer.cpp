9c2618f30e1b0b630fcf4ecbfed5a640c83c7a7a
Nils Daumann <nils_daumann@slindev.com>
2017-03-12 19:34:23 +0100

    anisotropic filtering.

--- Modules/D3D12/Sources/RND3D12Renderer.cpp
+++ Modules/D3D12/Sources/RND3D12Renderer.cpp
@@ -62,7 +62,7 @@ namespace RN
 
 			// Create sampler
 			D3D12_STATIC_SAMPLER_DESC samplerDesc = {};
-			samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+			samplerDesc.Filter = D3D12_FILTER_ANISOTROPIC;//D3D12_FILTER_MIN_MAG_MIP_LINEAR;
 			samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
 			samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
 			samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
@@ -70,7 +70,7 @@ namespace RN
 			samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
 			samplerDesc.MinLOD = 0.0f;
 			samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
-			samplerDesc.MaxAnisotropy = 0;
+			samplerDesc.MaxAnisotropy = 16;
 			samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK;
 			samplerDesc.ShaderRegister = 0;
 			samplerDesc.RegisterSpace = 0;
@@ -189,7 +189,7 @@ namespace RN
 
 		// Create sampler
 		D3D12_STATIC_SAMPLER_DESC samplerDesc = {};
-		samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.Filter = D3D12_FILTER_MIN_MAG_LINEAR_MIP_POINT;
 		samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
 		samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
 		samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
@@ -197,7 +197,7 @@ namespace RN
 		samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
 		samplerDesc.MinLOD = 0.0f;
 		samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
-		samplerDesc.MaxAnisotropy = 0;
+		samplerDesc.MaxAnisotropy = 16;
 		samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK;
 		samplerDesc.ShaderRegister = 0;
 		samplerDesc.RegisterSpace = 0;
