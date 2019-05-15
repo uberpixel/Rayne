f26f22ec4863e57dd481ca012bae86ce1ace4ba9
Nils Daumann <nils_daumann@slindev.com>
2019-05-15 11:49:50 +0200

    Basic visibility list based scene implementation.

--- Source/Scene/RNSceneWithVisibilityLists.cpp
+++ Source/Scene/RNSceneWithVisibilityLists.cpp
@@ -13,19 +13,39 @@
 #include "../Objects/RNAutoreleasePool.h"
 
 #define kRNSceneUpdateBatchSize 64
-#define kRNSceneRenderBatchSize 32
 
 namespace RN
 {
 	RNDefineMeta(SceneWithVisibilityLists, Scene)
-
-	SceneWithVisibilityLists::SceneWithVisibilityLists()
+	RNDefineScopedMeta(SceneWithVisibilityLists, Volume, Object)
+	RNDefineScopedMeta(SceneWithVisibilityLists, AxisAlignedBoxVolume, SceneWithVisibilityLists::Volume)
+	RNDefineMeta(SceneWithVisibilityListsInfo, SceneInfo)
+	
+	SceneWithVisibilityListsInfo::SceneWithVisibilityListsInfo(Scene *scene) : SceneInfo(scene)
 	{
 		
 	}
+	
+	bool SceneWithVisibilityLists::Volume::ContainsCamera(Camera *camera) const
+	{
+		return true;
+	}
+	
+	bool SceneWithVisibilityLists::AxisAlignedBoxVolume::ContainsCamera(Camera *camera) const
+	{
+		const Vector3 &cameraPosition = camera->GetWorldPosition();
+		return (cameraPosition.x >= boundsMin.x && cameraPosition.y >= boundsMin.y && cameraPosition.z >= boundsMin.z && cameraPosition.x <= boundsMax.x && cameraPosition.y <= boundsMax.y && cameraPosition.z <= boundsMax.z);
+	}
+
+	SceneWithVisibilityLists::SceneWithVisibilityLists() : _isAddingVolume(false)
+	{
+		_volumes = new Array();
+		_defaultVolume = new Volume();
+	}
 	SceneWithVisibilityLists::~SceneWithVisibilityLists()
 	{
-		
+		SafeRelease(_volumes);
+		SafeRelease(_defaultVolume);
 	}
 
 	void SceneWithVisibilityLists::Update(float delta)
@@ -108,11 +128,22 @@ namespace RN
 		DidUpdate(delta);
 	}
 	
+	void SceneWithVisibilityLists::RenderVolumeList(int drawPriority, Renderer *renderer, Camera *camera, Volume *volume)
+	{
+		for(SceneNode *node : volume->nodes)
+		{
+			//Skip if this is not the nodes draw priority or other reason (node is a light for example)
+			if((drawPriority == 0 && node->IsKindOfClass(Light::GetMetaClass())) || (drawPriority == 1 && (node->GetFlags() & SceneNode::Flags::DrawEarly)) || (drawPriority == 2 && !(node->GetFlags() & SceneNode::Flags::DrawEarly || node->GetFlags() & SceneNode::Flags::DrawLate)) || (drawPriority == 3 && (node->GetFlags() & SceneNode::Flags::DrawLate)))
+			{
+				if(node->CanRender(renderer, camera))
+					node->Render(renderer, camera);
+			}
+		}
+	}
+	
 	void SceneWithVisibilityLists::Render(Renderer *renderer)
 	{
 		WillRender(renderer);
-		
-		WorkQueue *queue = WorkQueue::GetGlobalQueue(WorkQueue::Priority::Default);
 		
 		for(int cameraPriority = 0; cameraPriority < 3; cameraPriority++)
 		{
@@ -120,7 +151,6 @@ namespace RN
 			while(member)
 			{
 				Camera *camera = member->Get();
-				camera->PostUpdate(renderer);
 				
 				//Early out if camera is not supposed to render or if this isn't it's priority loop
 				if(camera->GetFlags() & Camera::Flags::NoRender || (cameraPriority == 0 && !(camera->GetFlags() & Camera::Flags::RenderEarly)) || (cameraPriority == 1 && (camera->GetFlags() & Camera::Flags::RenderEarly || camera->GetFlags() & Camera::Flags::RenderLate)) || (cameraPriority == 2 && !(camera->GetFlags() & Camera::Flags::RenderLate)))
@@ -129,80 +159,26 @@ namespace RN
 					continue;
 				}
 				
+				camera->PostUpdate(renderer);
+				
+				Volume *volume = nullptr;
+				_volumes->Enumerate<Volume>([&](Volume *_volume, size_t index, bool &stop){
+					if(_volume->ContainsCamera(camera))
+					{
+						volume = _volume;
+						stop = true;
+					}
+				});
+				
 				renderer->SubmitCamera(camera, [&] {
-					WorkGroup *group = new WorkGroup();
-					
 					for(int drawPriority = 0; drawPriority < 4; drawPriority++)
 					{
-						for(size_t i = 0; i < 3; i ++)
+						RenderVolumeList(drawPriority, renderer, camera, _defaultVolume);
+						if(volume)
 						{
-							IntrusiveList<SceneNode>::Member *member = _nodes[i].GetHead();
-							IntrusiveList<SceneNode>::Member *first = member;
-							
-							size_t count = 0;
-							
-							while(member)
-							{
-								if(count == kRNSceneRenderBatchSize)
-								{
-									group->Perform(queue, [&, member, first] {
-										
-										AutoreleasePool pool;
-										auto iterator = first;
-										
-										while(iterator != member)
-										{
-											SceneNode *node = iterator->Get();
-											
-											//Skip if this is not the nodes draw priority or other reason (node is a light for example)
-											if((drawPriority == 0 && node->IsKindOfClass(Light::GetMetaClass())) || (drawPriority == 1 && (node->GetFlags() & SceneNode::Flags::DrawEarly)) || (drawPriority == 2 && !(node->GetFlags() & SceneNode::Flags::DrawEarly || node->GetFlags() & SceneNode::Flags::DrawLate)) || (drawPriority == 3 && (node->GetFlags() & SceneNode::Flags::DrawLate)))
-											{
-												if(node->CanRender(renderer, camera))
-													node->Render(renderer, camera);
-											}
-											
-											iterator = iterator->GetNext();
-										}
-									});
-									
-									first = member;
-									count = 0;
-								}
-								
-								member = member->GetNext();
-								count ++;
-							}
-							
-							//Render remaining less than kRNSceneRenderBatchSize number of nodes
-							if(first != member)
-							{
-								group->Perform(queue, [&, member, first] {
-									
-									AutoreleasePool pool;
-									auto iterator = first;
-									
-									while(iterator != member)
-									{
-										SceneNode *node = iterator->Get();
-										
-										//Skip if this is not the nodes draw priority or other reason (node is a light for example)
-										if((drawPriority == 0 && node->IsKindOfClass(Light::GetMetaClass())) || (drawPriority == 1 && (node->GetFlags() & SceneNode::Flags::DrawEarly)) || (drawPriority == 2 && !(node->GetFlags() & SceneNode::Flags::DrawEarly || node->GetFlags() & SceneNode::Flags::DrawLate)) || (drawPriority == 3 && (node->GetFlags() & SceneNode::Flags::DrawLate)))
-										{
-											if(node->CanRender(renderer, camera))
-												node->Render(renderer, camera);
-										}
-										
-										iterator = iterator->GetNext();
-									}
-									
-								});
-							}
+							RenderVolumeList(drawPriority, renderer, camera, volume);
 						}
-						
-						group->Wait();
 					}
-					
-					group->Release();
 				});
 				
 				member = member->GetNext();
@@ -210,6 +186,33 @@ namespace RN
 		}
 		
 		DidRender(renderer);
+	}
+	
+	void SceneWithVisibilityLists::AddVolume(Volume *volume)
+	{
+		_volumes->AddObject(volume);
+		
+		_isAddingVolume = true;
+		for(SceneNode *node : volume->nodes)
+		{
+			if(!node->_sceneInfo)
+				AddNode(node);
+			
+			SceneWithVisibilityListsInfo *sceneInfo = node->GetSceneInfo()->Downcast<SceneWithVisibilityListsInfo>();
+			
+			if(sceneInfo->volumes.size() == 1 && sceneInfo->volumes[0] == _defaultVolume)
+			{
+				auto iterator = std::find(_defaultVolume->nodes.begin(), _defaultVolume->nodes.end(), node);
+				if(iterator != _defaultVolume->nodes.end())
+				{
+					_defaultVolume->nodes.erase(iterator);
+				}
+				sceneInfo->volumes.pop_back();
+			}
+			
+			sceneInfo->volumes.push_back(volume);
+		}
+		_isAddingVolume = false;
 	}
 	
 	void SceneWithVisibilityLists::AddNode(SceneNode *node)
@@ -223,9 +226,16 @@ namespace RN
 		}
 		
 		_nodes[static_cast<size_t>(node->GetPriority())].PushBack(node->_sceneEntry);
-		
+			
 		node->Retain();
-		node->UpdateSceneInfo(new SceneInfo(this));
+		SceneWithVisibilityListsInfo *sceneInfo = new SceneWithVisibilityListsInfo(this);
+		node->UpdateSceneInfo(sceneInfo->Autorelease());
+		
+		if(!_isAddingVolume)
+		{
+			_defaultVolume->nodes.push_back(node);
+			sceneInfo->volumes.push_back(_defaultVolume);
+		}
 	}
 	
 	void SceneWithVisibilityLists::RemoveNode(SceneNode *node)
@@ -239,6 +249,16 @@ namespace RN
 		}
 		
 		_nodes[static_cast<size_t>(node->GetPriority())].Erase(node->_sceneEntry);
+		
+		SceneWithVisibilityListsInfo *sceneInfo = node->GetSceneInfo()->Downcast<SceneWithVisibilityListsInfo>();
+		for(Volume *volume : sceneInfo->volumes)
+		{
+			auto iterator = std::find(volume->nodes.begin(), volume->nodes.end(), node);
+			if(iterator != volume->nodes.end())
+			{
+				volume->nodes.erase(iterator);
+			}
+		}
 		
 		node->UpdateSceneInfo(nullptr);
 		node->Autorelease();
--- Source/Scene/RNSceneWithVisibilityLists.h
+++ Source/Scene/RNSceneWithVisibilityLists.h
@@ -16,7 +16,29 @@ namespace RN
 	class SceneWithVisibilityLists : public Scene
 	{
 	public:
+		class Volume : public Object
+		{
+		public:
+			RNAPI virtual bool ContainsCamera(Camera *camera) const;
+			std::vector<SceneNode *> nodes;
+			
+			__RNDeclareMetaInternal(Volume)
+		};
+		
+		class AxisAlignedBoxVolume : public Volume
+		{
+		public:
+			RNAPI bool ContainsCamera(Camera *camera) const final;
+			
+			Vector3 boundsMin;
+			Vector3 boundsMax;
+			
+			__RNDeclareMetaInternal(AxisAlignedBoxVolume)
+		};
+		
 		RNAPI ~SceneWithVisibilityLists();
+		
+		RNAPI void AddVolume(Volume *volume);
 		
 		RNAPI void AddNode(SceneNode *node) override;
 		RNAPI void RemoveNode(SceneNode *node) override;
@@ -26,11 +48,28 @@ namespace RN
 
 		RNAPI void Update(float delta) override;
 		RNAPI void Render(Renderer *renderer) override;
+		
+		RNAPI void RenderVolumeList(int drawPriority, Renderer *renderer, Camera *camera, Volume *volume);
 
 		IntrusiveList<SceneNode> _nodes[3];
 		IntrusiveList<Camera> _cameras;
+		
+		Array *_volumes;
+		Volume *_defaultVolume;
+		
+		bool _isAddingVolume;
 
 		__RNDeclareMetaInternal(SceneWithVisibilityLists)
+	};
+	
+	class SceneWithVisibilityListsInfo : public SceneInfo
+	{
+	public:
+		SceneWithVisibilityListsInfo(Scene *scene);
+		
+		std::vector<SceneWithVisibilityLists::Volume *>volumes;
+		
+		__RNDeclareMetaInternal(SceneWithVisibilityListsInfo)
 	};
 }
 
