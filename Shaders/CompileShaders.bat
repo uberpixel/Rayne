6fd3012166aaac4bc2da555dbd54a8118fdb6da7
Nils Daumann <nils_daumann@slindev.com>
2020-04-04 20:17:38 +0200

    D3D12 shader update.

--- Shaders/CompileShaders.bat
+++ Shaders/CompileShaders.bat
@@ -1,3 +1,4 @@
 cd /D "%~dp0"
 @RD /S /Q "../Modules/Vulkan/Resources"
-python ../Tools/ShaderProcessor/convert.py Shadows/Shaders.json spirv ../Modules/Vulkan/Resources ":RayneVulkan:"
+start python ../Tools/ShaderProcessor/convert.py Shadows/Shaders.json spirv ../Modules/Vulkan/Resources ":RayneVulkan:"
+start python ../Tools/ShaderProcessor/convert.py Shadows/Shaders.json cso ../Modules/D3D12/Resources ":RayneD3D12:"
