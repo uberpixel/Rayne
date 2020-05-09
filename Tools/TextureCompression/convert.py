c6a53d964737406d0cd733ea2df1ddd7d875227e
Nils Daumann <nils_daumann@slindev.com>
2020-05-09 03:32:59 +0200

    Updated astcenc module and the converter script to do the y flip correctly.

--- Tools/TextureCompression/convert.py
+++ Tools/TextureCompression/convert.py
@@ -152,7 +152,9 @@ def main():
 
             print 'Number of mipmap levels: ' + str(numLevels)
 
-            subprocess.call(['convert', sourceFile, '-flip', '-define', 'png:bit-depth='+str(bitdepth), outputFileName + '.0' + inputFileExtension])
+            #the encoder now seems to do the flipping itself, it also has a flip related flag, but doesn't appear to be needed
+            #subprocess.call(['convert', sourceFile, '-flip', '-define', 'png:bit-depth='+str(bitdepth), outputFileName + '.0' + inputFileExtension])
+            subprocess.call(['convert', sourceFile, '-define', 'png:bit-depth='+str(bitdepth), outputFileName + '.0' + inputFileExtension])
             for i in range(1, numLevels):
                 subprocess.call(['convert', outputFileName + '.' + str(i-1) + inputFileExtension, '-scale', '50%', '-define', 'png:bit-depth='+str(bitdepth), outputFileName + '.' + str(i) + inputFileExtension])
 
