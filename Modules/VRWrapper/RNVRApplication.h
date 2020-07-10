16af0cb3047b779f868ea68250427869bbacfab0
Nils Daumann <nils_daumann@slindev.com>
2020-07-11 01:42:49 +0200

    Added a hacky „VRApplication“ to the VR module to abstract a couple of mostly VR specific things.

---
+++ Modules/VRWrapper/RNVRApplication.h
@@ -0,0 +1,54 @@
+//
+//  RNVRApplication.h
+//  Rayne-VR
+//
+//  Copyright 2020 by SlinDev. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and corona.
+//
+
+#ifndef __RAYNE_VRAPPLICATION_H_
+#define __RAYNE_VRAPPLICATION_H_
+
+#include <Rayne.h>
+#include "RNVRWindow.h"
+
+//TODO: These should be defines somehow set by the corresponding vr module....
+#if RN_PLATFORM_ANDROID
+    #include "RNOculusMobileWindow.h"
+#elif RN_PLATFORM_WINDOWS
+    #ifdef BUILD_FOR_OCULUS
+        #include "RNOculusWindow.h"
+    #else
+        #include "RNOculusWindow.h"
+        #include "RNOpenVRWindow.h"
+    #endif
+#else
+    #include "RNOpenVRWindow.h"
+#endif
+
+namespace RN
+{
+	class VRApplication : public Application
+	{
+	public:
+		VRApplication();
+		~VRApplication() override;
+		
+		virtual RendererDescriptor *GetPreferredRenderer() const override;
+		virtual RenderingDevice *GetPreferredRenderingDevice(RendererDescriptor *descriptor, const Array *devices) const override;
+
+		virtual void WillFinishLaunching(Kernel *kernel) override;
+		virtual void DidFinishLaunching(Kernel *kernel) override;
+		
+		VRWindow *GetVRWindow() const  { return _vrWindow; }
+
+    private:
+        void SetupVR();
+        VRWindow *_vrWindow;
+		
+		RNDeclareMeta(VRApplication)
+	};
+}
+
+
+#endif /* __RAYNE_VRAPPLICATION_H_ */
---
+++ Modules/VRWrapper/RNVRApplicationImpl.h
@@ -0,0 +1,156 @@
+//
+//  RNVRApplication.cpp
+//  Rayne-VR
+//
+//  Copyright 2020 by SlinDev. All rights reserved.
+//  Unauthorized use is punishable by torture, mutilation, and corona.
+//
+
+#include "RNVRApplication.h"
+
+namespace RN
+{
+	RNDefineMeta(VRApplication, Application)
+
+	VRApplication::VRApplication() : _vrWindow(nullptr)
+	{
+		
+	}
+
+	VRApplication::~VRApplication()
+	{
+		SafeRelease(_vrWindow);
+	}
+
+	void VRApplication::SetupVR()
+	{
+		if(_vrWindow)
+			return;
+
+	#if RN_PLATFORM_ANDROID
+		_vrWindow = new RN::OculusMobileWindow();
+	#else
+		bool wantsOpenVR = RN::Kernel::GetSharedInstance()->GetArguments().HasArgument("openvr", 0);
+	#if RN_PLATFORM_WINDOWS
+		bool wantsOculus = RN::Kernel::GetSharedInstance()->GetArguments().HasArgument("oculusvr", 0);
+
+		#ifndef BUILD_FOR_OCULUS
+		if(!wantsOpenVR && (!RN::OpenVRWindow::IsSteamVRRunning() || wantsOculus))
+		#endif
+		{
+			if(RN::OculusWindow::GetAvailability() == RN::VRWindow::HMD || wantsOculus)
+			{
+				_vrWindow = new RN::OculusWindow();
+				return;
+			}
+		}
+	#endif
+
+	#ifndef BUILD_FOR_OCULUS
+		if(RN::OpenVRWindow::GetAvailability() == RN::VRWindow::HMD || wantsOpenVR)
+		{
+			_vrWindow = new RN::OpenVRWindow();
+			return;
+		}
+	#endif
+	#endif
+	}
+
+	RendererDescriptor *VRApplication::GetPreferredRenderer() const
+	{
+		if(!_vrWindow) return RN::Application::GetPreferredRenderer();
+
+		RN::Array *instanceExtensions = _vrWindow->GetRequiredVulkanInstanceExtensions();
+		RN::Dictionary *parameters = nullptr;
+		if(instanceExtensions)
+		{
+			parameters = new RN::Dictionary();
+			parameters->SetObjectForKey(instanceExtensions, RNCSTR("instanceextensions"));
+		}
+
+	#if RN_PLATFORM_ANDROID
+		RN::RendererDescriptor *descriptor = RN::RendererDescriptor::GetRendererWithIdentifier(RNCSTR("net.uberpixel.rendering.vulkan"), parameters);
+	#else
+		RN::RendererDescriptor *descriptor = RN::RendererDescriptor::GetPreferredRenderer(parameters);
+	#endif
+		return descriptor;
+	}
+
+	RN::RenderingDevice *VRApplication::GetPreferredRenderingDevice(RN::RendererDescriptor *descriptor, const RN::Array *devices) const
+	{
+		if(!_vrWindow) return RN::Application::GetPreferredRenderingDevice(descriptor, devices);
+
+		RN::RenderingDevice *preferred = nullptr;
+
+		RN::RenderingDevice *vrDevice = _vrWindow->GetOutputDevice(descriptor);
+		if(vrDevice)
+		{
+			devices->Enumerate<RN::RenderingDevice>([&](RN::RenderingDevice *device, size_t index, bool &stop) {
+				if(vrDevice->IsEqual(device))
+				{
+					preferred = device;
+					stop = true;
+				}
+			});
+		}
+
+		if(!preferred)
+			preferred = RN::Application::GetPreferredRenderingDevice(descriptor, devices);
+
+		RN::Array *deviceExtensions = _vrWindow->GetRequiredVulkanDeviceExtensions(descriptor, preferred);
+		preferred->SetExtensions(deviceExtensions);
+
+		return preferred;
+	}
+
+	void VRApplication::WillFinishLaunching(RN::Kernel *kernel)
+	{
+		RN::Application::WillFinishLaunching(kernel);
+		
+		if(!RN::Kernel::GetSharedInstance()->GetArguments().HasArgument("pancake", '2d'))
+		{
+			SetupVR();
+		}
+	}
+	
+	void VRApplication::DidFinishLaunching(RN::Kernel *kernel)
+	{
+		RN::Application::DidFinishLaunching(kernel);
+		
+		if(_vrWindow)
+		{
+			RN::Renderer *renderer = RN::Renderer::GetActiveRenderer();
+			renderer->SetMainWindow(_vrWindow);
+
+			RN::Window::SwapChainDescriptor swapChainDescriptor;
+#if RN_PLATFORM_WINDOWS
+			if(_vrWindow->IsKindOfClass(RN::OculusWindow::GetMetaClass()))
+			{
+				swapChainDescriptor.depthStencilFormat = RN::Texture::Format::Depth_32F;
+				swapChainDescriptor.colorFormat = RN::Texture::Format::RGBA_16F;
+			}
+#endif
+			_vrWindow->StartRendering(swapChainDescriptor);
+		}
+		else
+		{
+			kernel->SetMaxFPS(200.0f);
+			
+			RN::Window::SwapChainDescriptor swapchainDescriptor(RN::Texture::Format::RGBA_16F, RN::Texture::Format::Depth_32F);
+			swapchainDescriptor.vsync = false;
+			
+			RN::Vector2 windowSize(1024, 768);
+			RN::Dictionary *resolutionDictionary = RN::Settings::GetSharedInstance()->GetEntryForKey<RN::Dictionary>(RNCSTR("RNResolution"));
+			if(resolutionDictionary)
+			{
+				RN::Number *widthNumber = resolutionDictionary->GetObjectForKey<RN::Number>(RNCSTR("width"));
+				RN::Number *heightNumber = resolutionDictionary->GetObjectForKey<RN::Number>(RNCSTR("height"));
+				if(widthNumber) windowSize.x = widthNumber->GetInt32Value();
+				if(heightNumber) windowSize.y = heightNumber->GetInt32Value();
+			}
+			
+			RN::Window *previewWindow = RN::Renderer::GetActiveRenderer()->CreateAWindow(windowSize, RN::Screen::GetMainScreen(), swapchainDescriptor);
+			previewWindow->Show();
+		}
+	}
+}
