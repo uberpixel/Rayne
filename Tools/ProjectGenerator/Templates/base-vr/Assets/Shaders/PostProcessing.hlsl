f4568f2d546186a652aa6265a1db4653b133fc5b
Nils Daumann <nils_daumann@slindev.com>
2020-10-09 01:15:32 +0200

    Updated template post processing shader to not use explicit bindings.

--- Tools/ProjectGenerator/Templates/base-vr/Assets/Shaders/PostProcessing.hlsl
+++ Tools/ProjectGenerator/Templates/base-vr/Assets/Shaders/PostProcessing.hlsl
@@ -5,10 +5,10 @@
 //  Copyright __TMP_YEAR__ by __TMP_COMPANY__. All rights reserved.
 //
 
-[[vk::binding(3)]] SamplerState linearRepeatSampler : register(s0);
-[[vk::binding(4)]] Texture2D texture0 : register(t0);
+SamplerState linearRepeatSampler;
+Texture2D framebufferTexture;
 
-[[vk::binding(1)]] cbuffer vertexUniforms : register(b0)
+cbuffer vertexUniforms
 {
 	float4 diffuseColor;
 	float4 ambientColor;
@@ -37,6 +37,6 @@ FragmentVertex pp_vertex(InputVertex vert)
 
 float4 pp_blit_fragment(FragmentVertex vert) : SV_TARGET
 {
-	float4 color = texture0.Sample(linearRepeatSampler, vert.texCoords).rgba * ambientColor.rgba;
+	float4 color = framebufferTexture.Sample(linearRepeatSampler, vert.texCoords).rgba * ambientColor.rgba;
 	return color;
 }
