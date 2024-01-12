be92f5e0dccf9a7599079d5502866ce3a9b5c9fa
Nils Daumann <nils_daumann@slindev.com>
2024-01-12 13:36:17 +0100

    Added metal shaders for mac.

--- Shaders/CleanShaders.sh
+++ Shaders/CleanShaders.sh
@@ -1,7 +1,7 @@
 cd $(dirname "$BASH_SOURCE")
-#rm -rf ../Modules/Vulkan/Resources
-#rm -rf ../Modules/Metal/Resources_macos
-#rm -rf ../Modules/Metal/Resources_ios
-#rm -rf ../Modules/Metal/Resources_ios_sim
+rm -rf ../Modules/Vulkan/Resources
+rm -rf ../Modules/Metal/Resources_macos
+rm -rf ../Modules/Metal/Resources_ios
+rm -rf ../Modules/Metal/Resources_ios_sim
 rm -rf ../Modules/Metal/Resources_visionos
 rm -rf ../Modules/Metal/Resources_visionos_sim
